54977b8395041a1599e75ef88ed76f44bd759265
Juanes <kuronekosan1614@gmail.com>
2025-08-25 16:19:16 -0500

    Fix rutas de archivos estáticos

--- blogapi/urls.py
+++ blogapi/urls.py
@@ -14,15 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path, include
-
 from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls')),
-    path('', lambda request: redirect('api/')),  # redirige al API
+    path('', lambda request: redirect('api/')),  # redirección inicial
 ]
+
+# 👇 Esto permite servir archivos estáticos (CSS, JS) correctamente
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
